fca060201f46424abfe8c09e2e218ff534c51d71
petro2323 <aleksandarpetronijevic65@gmail.com>
2024-03-12 15:19:33 +0100

    implementirana enkripcija u registraciji

--- project-root/app/Controllers/Register.php
+++ project-root/app/Controllers/Register.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 use App\Models\Users;
+use App\Config\Encryption;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
@@ -11,11 +12,17 @@ class Register extends BaseController
     
 protected $usersModel;
 protected $validation;
+protected $key;
+protected $iv;
+protected $cipher;
 
     public function __construct()
     {
         $this->usersModel = new Users();
         $this->validation = \Config\Services::validation();
+        $this->key = \Config\Encryption::$key;
+        $this->iv = \Config\Encryption::$iv;
+        $this->cipher = \Config\Encryption::$cipher;
     }
     
     public function index()
@@ -32,16 +39,19 @@ protected $validation;
                 'validation' => $this->validation
             ]); //treba izmjenit da se pojavi poruka o gresci
         } else {
+             
             $data = [
-                'first_name' => $this->request->getPost('fName'),
-                'last_name' => $this->request->getPost('lName'),
-                'email' => $this->request->getPost('email'),
+                'first_name' => openssl_encrypt($this->request->getPost('fName'), $this->cipher, $this->key, 0, $this->iv),
+                'last_name' => openssl_encrypt($this->request->getPost('lName'), $this->cipher, $this->key, 0, $this->iv),
+                'email' => openssl_encrypt($this->request->getPost('email'), $this->cipher, $this->key, 0, $this->iv),
                 'username' => $this->request->getPost('username'),
                 'password' => password_hash($this->request->getPost('password'), PASSWORD_DEFAULT), 
                 'user_type_id' => 3
             ];
 
-            if($this->usersModel->where('email', $data['email'])->countAllResults() > 0) {
+            if($this->usersModel->where('username', $data['username'])->countAllResults() > 0) {
+                return redirect()->to(base_url('register')); //treba izmjenit da se pojavi poruka o gresci   
+            } else if($this->usersModel->where('email', $data['email'])->countAllResults() > 0) {
                 return redirect()->to(base_url('register')); //treba izmjenit da se pojavi poruka o gresci
             } else {
                 $this->usersModel->insert($data);
