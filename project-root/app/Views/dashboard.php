5902134fc980d78a597e7455fd5607d106fcc8e3
andrijadragojevic <andrijadrago55@gmail.com>
2024-05-07 11:52:29 +0200

    proljepsao cart meni i dodao logiku za login i logout u navbaru

--- project-root/app/Views/dashboard.php
+++ project-root/app/Views/dashboard.php
@@ -94,16 +94,32 @@
 
         <div class="collapse navbar-collapse " id="navbar-list-4">
             <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="<?=base_url('/photos/user-icon.png')?>" width="40" height="40" class="rounded-circle">
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Dashboard</a>
-                        <a class="dropdown-item" href="#">Edit Profile</a>
-                        <a class="dropdown-item" href="<?=base_url('logout')?>">Log Out</a>
-                    </div>
-                </li>   
+                <?php
+                $session = \Config\Services::session();
+                if ($session->has('username')) {
+                echo '<li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="' . base_url('/photos/user-icon.png') . '" width="40" height="40" class="rounded-circle">
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="' . base_url('dashboard') . '">Dashboard</a>
+                            <a class="dropdown-item" href="#">Edit Profile</a>
+                            <a class="dropdown-item" href="' . base_url('logout') . '">Logout</a>
+                        </div>
+                    </li>';
+            } else {
+                echo '<li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="' . base_url('/photos/user-icon.png') . '" width="40" height="40" class="rounded-circle">
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="' . base_url('dashboard') . '">Dashboard</a>
+                            <a class="dropdown-item" href="#">Edit Profile</a>
+                            <a class="dropdown-item" href="' . base_url('login') . '">Login</a>
+                        </div>
+                    </li>';
+            }
+                ?>
             </ul>
     </ul>
 </nav>
