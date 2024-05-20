955aee94d18c37e9e8d7faf8e7bf5a9a2e67c3de
petro2323 <aleksandarpetronijevic65@gmail.com>
2024-05-20 19:13:13 +0200

    implementacija voucher.js part II

--- project-root/public/js/address.js
+++ project-root/public/js/address.js
@@ -49,29 +49,29 @@ deleteBtn.addEventListener('click', () => {
 
 
 function setCookie(name, value, days) {
-    let expires = "";
+    let expires = ""
     if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
+        let date = new Date()
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+        expires = "; expires=" + date.toUTCString()
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    document.cookie = name + "=" + (value || "") + expires + "; path=/"
 }
 
 
 function getCookie(name) {
-    let nameEQ = name + "=";
-    let ca = document.cookie.split(';');
+    let nameEQ = name + "="
+    let ca = document.cookie.split(';')
     for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        let c = ca[i]
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length)
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
     }
-    return null;
+    return null
 }
 
 function deleteCookie(name) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "/erase-cookie/" + name, true);
-    xhr.send();
+    let xhr = new XMLHttpRequest()
+    xhr.open("GET", "/erase-cookie/" + name, true)
+    xhr.send()
 }
--- project-root/public/js/cart.js
+++ project-root/public/js/cart.js
@@ -1,44 +1,35 @@
 let cart = {}
+let voucherStatus = false
 
 function addToCart(title, price, image_url) {
     if (!cart[title]) {
         cart[title] = {amount: 1, price: price, image_url: image_url}
     }
 
-    renderCart()
+    viewCart()
 
     if (!document.getElementById('cart').checked) {
         document.getElementById('cart').checked = true
     }
 }
 
+function viewCart() {
+    if (voucherStatus) {
+        renderCart(voucherStatus)
+    } else {
+        renderCart()
+    }
+}
+
 document.getElementById('voucher_button').addEventListener("click", () => {
     if (Object.keys(cart).length > 0) {
-        const codes = {
-            "trop-delivery-2002": 0.2,
-            "onin-majstor-256": 0.3,
-            "fit-mediteran-06": 0.5,
-            "skendza-car-99": 0.6,
-            "mini-cooper-bean": 0.1
-        }
-    
-        let input = document.getElementById('trop_voucher')
-        let voucherInput = input.value
-    
-        if (codes.hasOwnProperty(voucherInput)) {
-            let discount = codes[voucherInput]
-            let finalPrice = document.getElementById('final-price')
-            let finalPriceBeforeVoucher = parseFloat(finalPrice.innerHTML)
-    
-            finalPrice.innerHTML = `${(finalPriceBeforeVoucher - (finalPriceBeforeVoucher * discount)).toFixed(2)} €`
-
-            document.getElementById('voucher_button').style.display = 'none'
-            input.disabled = true
-        }
+        voucherStatus = true
     }
+    
+    viewCart()
 })
 
-function renderCart() {
+function renderCart(voucher = false) {
     let cartItems = document.getElementById('cart-items')
     let totalPrice = document.getElementById('total-price')
     let pdv = document.getElementById('pdv')
@@ -69,54 +60,94 @@ function renderCart() {
         cartItems.appendChild(itemDiv)
     }
 
-    let pdvPrice = price * 0.10
-    let deliveryPrice = 0
-    if (Object.keys(cart).length > 0) {
-        deliveryPrice = 5
-    }
-    let sum = price + pdvPrice + deliveryPrice
+    if (voucher) {
+        const codes = {
+            "trop-delivery-2002": 0.2,
+            "onin-majstor-256": 0.3,
+            "fit-mediteran-06": 0.5,
+            "skendza-car-99": 0.6,
+            "mini-cooper-bean": 0.1
+        }
+        
+        let input = document.getElementById('trop_voucher')
+        let voucherInput = input.value
+    
+        if (codes.hasOwnProperty(voucherInput)) {
+            let discount = codes[voucherInput]
 
-    totalPrice.innerHTML = `${price.toFixed(2)} €`
-    pdv.innerHTML = `${pdvPrice.toFixed(2)} €`
-    if (deliveryPrice == 5) {
-        delivery.innerHTML = `${deliveryPrice.toFixed(2)} €`
+            let discountPrice = price - (price * discount)
+            
+            let pdvPrice = discountPrice * 0.10
+            let deliveryPrice = 0
+            if (Object.keys(cart).length > 0) {
+                deliveryPrice = 5 - (5 * discount)
+            }
+            let sum = discountPrice + pdvPrice + deliveryPrice
+
+            totalPrice.innerHTML = `${discountPrice.toFixed(2)} €`
+            pdv.innerHTML = `${pdvPrice.toFixed(2)} €`
+            if (deliveryPrice == (5 - (5 * discount))) {
+                delivery.innerHTML = `${deliveryPrice.toFixed(2)} €`
+            } else {
+                delivery.innerHTML = ''
+            }
+            finalPrice.innerHTML = `${sum.toFixed(2)} €`
+
+            document.getElementById('voucher_button').style.display = 'none'
+            input.disabled = true
+        } else {
+            voucherStatus = false
+        }
     } else {
-        delivery.innerHTML = ''
+        let pdvPrice = price * 0.10
+        let deliveryPrice = 0
+        if (Object.keys(cart).length > 0) {
+            deliveryPrice = 5
+        }
+        let sum = price + pdvPrice + deliveryPrice
+
+        totalPrice.innerHTML = `${price.toFixed(2)} €`
+        pdv.innerHTML = `${pdvPrice.toFixed(2)} €`
+        if (deliveryPrice == 5) {
+            delivery.innerHTML = `${deliveryPrice.toFixed(2)} €`
+        } else {
+            delivery.innerHTML = ''
+        }
+        finalPrice.innerHTML = `${sum.toFixed(2)} €`
     }
-    finalPrice.innerHTML = `${sum.toFixed(2)} €`
 }
 
 function increment(event, title) {
     event.stopPropagation()
     if (cart[title]) {
         cart[title].amount += 1
-        renderCart()
+        viewCart()
     }
 }
 
 function decrement(event, title) {
     event.stopPropagation();
     if (cart[title]) {
-        cart[title].amount -= 1;
+        cart[title].amount -= 1
         if (cart[title].amount < 1) {
-            delete cart[title];
+            delete cart[title]
         }
-        renderCart();
+        viewCart()
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    let cards = document.querySelectorAll('.dashboard-card');
+    let cards = document.querySelectorAll('.dashboard-card')
     
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            let title = card.querySelector('.title').innerText.trim();
-            let price = card.querySelector('.price').innerText.trim();
-            let image = card.querySelector('.card-image').src; 
+            let title = card.querySelector('.title').innerText.trim()
+            let price = card.querySelector('.price').innerText.trim()
+            let image = card.querySelector('.card-image').src;
             
-            price = parseFloat(price.replace('€', '').trim());
+            price = parseFloat(price.replace('€', '').trim())
 
-            addToCart(title, price, image);
+            addToCart(title, price, image)
         })
     })
 
