625d22a57eedab9732050714d1d6aec6a7595657
DrReg <nikolav706@gmail.com>
2024-06-06 18:09:34 +0200

    napravio nov dizajn tabele i dodao reklamu za history stranicu

--- project-root/app/Views/history.php
+++ project-root/app/Views/history.php
@@ -10,88 +10,87 @@
     <link rel="stylesheet" href="<?= base_url('css/login.css') ?>">
     <link rel="stylesheet" href="<?= base_url('css/main-page.css') ?>">
     <link rel="stylesheet" href="<?= base_url('css/history.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('css/dashboard.css') ?>">
     <link rel="icon" href="<?= base_url() ?>/photos/tropicon.png" type="image/gif">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <title>Trop Delivery</title>
 </head>
 
 <body>
-    
-<nav class="tropnav">
-    <div class="logo">
-        <img src="photos/TropDeliveryLogo.png" alt="Trop Delivery">
-        <span><a href="<?=base_url('/')?>">Trop Delivery</a></span>
-    </div>
-    <ul class="nav-links">
-    <div class="collapse navbar-collapse" id="navbar-list-4">
-        <ul class="navbar-nav">
-            <?php
-            $session = \Config\Services::session();
-            if ($session->has('username')) {
-            echo '<li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="' . base_url('/photos/user-icon.png') . '" width="40" height="40" class="rounded-circle">
-                    </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="' . base_url('dashboard') . '">Dashboard</a>
-                <a class="dropdown-item" href="#">Edit Profile</a>
-                <a class="dropdown-item" href="' . base_url('logout') . '">Logout</a>
-            </div>
-                </li>';
-        } else {
-            echo '<li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img src="' . base_url('/photos/user-icon.png') . '" width="40" height="40" class="rounded-circle">
-            </a>
-    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="' . base_url('dashboard') . '">Dashboard</a>
-        <a class="dropdown-item" href="#">Edit Profile</a>
-        <a class="dropdown-item" href="' . base_url('history') . '">History</a>
-        <a class="dropdown-item" href="' . base_url('login') . '">Login</a>
-    </div>
-        </li>';
-        }
-            ?>
-        </ul>
-    </div>
-</nav>
 
-    <div class="landing">
+    <?php include ('header.php') ?>
+
+    <div class="dashboard" id="history-advertizing">
+        <div class="history-banner">
+            <img src="<?= base_url('/food/nino-espresso-coffee-at-ninos.jpg') ?>" alt="burger">
+            <div class="banner-promo">
+                <h1><span>NOVO!!!</span>
+                    <br>
+                    Kupi kafu <br>
+                    Dobij šolju! <br>
+                </h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="landing" id="history-landing">
+
         <div class="main_container">
-            <table class="table" id="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
+
+        <h1 id="history-title">Pregled istorije</h1>
+
+            <div class="history-table-wrapper" id="rounded-order-history">
+                <table class="table" id="table">
+                    <thead>
+                        <tr>
+                            <th scope="col"># Tracking number</th>
+                            <th scope="col">Ime</th>
+                            <th scope="col">Prezime</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Naručene stavke</th>
+                            <th scope="col">Status porudžbine</th>
+                            <th scope="col">Ukupan iznos</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Nebojša</td>
+                            <td>Nedić</td>
+                            <td>nnedic98</td>
+                            <td></td>
+                            <td>U tranzitu</td>
+                            <td>8.59€</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Haris</td>
+                            <td>Adrović</td>
+                            <td>hakaizdzungle</td>
+                            <td></td>
+                            <td>Dostavljeno</td>
+                            <td>14.60€</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Miloš</td>
+                            <td>Sekulić</td>
+                            <td>selesekac87</td>
+                            <td></td>
+                            <td>Otkazano</td>
+                            <td>8.65€</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 
